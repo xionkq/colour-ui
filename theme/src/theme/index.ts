ca05598aed15cee0bb02cfdae12c4a9c169c58b0
xionkq <3030199850@qq.com>
2024-06-01 22:05:34 +0800

    feat: add theme color

--- theme/src/theme/index.ts
+++ theme/src/theme/index.ts
@@ -1,8 +1,11 @@
 export default {
   backgroundImage: {
-    'primary': 'linear-gradient(to right, #007cdf, #54e8a6, #ffe868, #ff6e54, #d72e90)',
+    'info': 'linear-gradient(to right, hsl(var(--info-color-light)), hsl(var(--info-color-dark)))',
+    'success': 'linear-gradient(to right, hsl(var(--success-color-light)), hsl(var(--success-color-dark)))',
+    'warning': 'linear-gradient(to right, hsl(var(--warning-color-light)), hsl(var(--warning-color-dark)))',
+    'error': 'linear-gradient(to right, hsl(var(--error-color-light)), hsl(var(--error-color-dark)))',
   },
   colors: {
-    'primary': 'rgb(var(--color-primary))'
+    'default': 'hsl(var(--default-bg-color))',
   },
 }
