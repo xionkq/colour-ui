9fd3066af9449a238e0987053997abb934f64b8f
xionkq <3030199850@qq.com>
2024-06-03 22:48:21 +0800

    feat: perfect gradation border!!

--- play/src/App.vue
+++ play/src/App.vue
@@ -12,5 +12,6 @@
   align-items: center;
   justify-content: center;
   height: 100vh;
+  background-image: repeating-linear-gradient(45deg, #ffffff, #ffffff 13px, #e5e6e6 13px, #e5e6e6 14px);
 }
 </style>
