87d08fbdc5d1284966d5c2bc775769f1b503c075
xionkq <3030199850@qq.com>
2024-06-04 23:06:33 +0800

    feat: optimize mask

--- play/src/App.vue
+++ play/src/App.vue
@@ -3,6 +3,10 @@
 <template>
   <div class="play-container">
     <c-button />
+<!--    <div class="mask-box">-->
+<!--      <div class="mask">11111</div>-->
+<!--      111111111111111111111111-->
+<!--    </div>-->
   </div>
 </template>
 
@@ -13,5 +17,21 @@
   justify-content: center;
   height: 100vh;
   background-image: repeating-linear-gradient(45deg, #ffffff, #ffffff 13px, #e5e6e6 13px, #e5e6e6 14px);
+}
+
+.mask-box {
+  background: aqua;
+  mask: linear-gradient(to right, #ffffffff, #00000000) border-box, linear-gradient(to right, #ffffffff, #00000000) content-box;
+  mask-composite: subtract;
+  border: 10px solid transparent;
+  padding: 10px;
+}
+
+.mask {
+  width: 100px;
+  height: 100px;
+  border: 10px solid transparent;
+  padding: 10px;
+  background: brown;
 }
 </style>
