4b2422587075cbf64a4cdc7f099da9e34b2d0a7e
xionkq <3030199850@qq.com>
2024-06-05 22:46:41 +0800

    feat: add gradient border to state button

--- theme/src/theme.ts
+++ theme/src/theme.ts
@@ -1,12 +1,17 @@
 export default {
   backgroundImage: {
-    default: 'hsl(var(--default-bg-color))',
     info: 'linear-gradient(to right, hsl(var(--info-color-light)), hsl(var(--info-color-dark)))',
     success: 'linear-gradient(to right, hsl(var(--success-color-light)), hsl(var(--success-color-dark)))',
     warning: 'linear-gradient(to right, hsl(var(--warning-color-light)), hsl(var(--warning-color-dark)))',
     error: 'linear-gradient(to right, hsl(var(--error-color-light)), hsl(var(--error-color-dark)))',
+
+    'info-hover': 'linear-gradient(to right, var(--info-hover-color-light), var(--info-hover-color-dark))',
+    'success-hover': 'linear-gradient(to right, var(--success-hover-color-light), var(--success-hover-color-dark))',
+    'warning-hover': 'linear-gradient(to right, var(--warning-hover-color-light), var(--warning-hover-color-dark))',
+    'error-hover': 'linear-gradient(to right, var(--error-hover-color-light), var(--error-hover-color-dark))',
   },
   colors: {
     default: 'hsl(var(--default-bg-color))',
+    'default-hover': 'var(--default-bg-hover-color)',
   },
 }
