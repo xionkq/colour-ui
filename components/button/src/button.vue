0ee8f945442f51615deedb67df3a52f3abd21da4
xionkq <3030199850@qq.com>
2024-06-16 21:13:10 +0800

    feat: add border button

--- components/button/src/button.vue
+++ components/button/src/button.vue
@@ -1,9 +1,18 @@
 <script setup></script>
 
 <template>
-  <button class="btn">button</button>
-  <button class="btn btn-info">button</button>
-  <button class="btn btn-success">button</button>
-  <button class="btn btn-warning">button</button>
-  <button class="btn btn-error">button</button>
+  <div>
+    <button class="btn">button</button>
+    <button class="btn btn-info">button</button>
+    <button class="btn btn-success">button</button>
+    <button class="btn btn-warning">button</button>
+    <button class="btn btn-error">button</button>
+  </div>
+  <div style="margin-top: 10px">
+    <button class="btn btn-outline">button</button>
+    <button class="btn btn-outline btn-info">button</button>
+    <button class="btn btn-outline btn-success">button</button>
+    <button class="btn btn-outline btn-warning">button</button>
+    <button class="btn btn-outline btn-error">button</button>
+  </div>
 </template>
--- play/src/App.vue
+++ play/src/App.vue
@@ -11,6 +11,7 @@
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
   height: 100vh;
   background-image: repeating-linear-gradient(45deg, #ffffff, #ffffff 13px, #e5e6e6 13px, #e5e6e6 14px);
 }
