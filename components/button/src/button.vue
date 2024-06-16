0777ff81d2c0ffc2bd10feec2b24663cbf8ccd9e
xionkq <3030199850@qq.com>
2024-06-16 22:05:19 +0800

    feat: add button of different sizes

--- components/button/src/button.vue
+++ components/button/src/button.vue
@@ -3,16 +3,22 @@
 <template>
   <div>
     <button class="btn">button</button>
-    <button class="btn btn-info">button</button>
-    <button class="btn btn-success">button</button>
-    <button class="btn btn-warning">button</button>
-    <button class="btn btn-error">button</button>
+    <button class="btn btn-info">info</button>
+    <button class="btn btn-success">success</button>
+    <button class="btn btn-warning">warning</button>
+    <button class="btn btn-error">error</button>
   </div>
   <div style="margin-top: 10px">
     <button class="btn btn-outline">button</button>
-    <button class="btn btn-outline btn-info">button</button>
-    <button class="btn btn-outline btn-success">button</button>
-    <button class="btn btn-outline btn-warning">button</button>
-    <button class="btn btn-outline btn-error">button</button>
+    <button class="btn btn-outline btn-info">info</button>
+    <button class="btn btn-outline btn-success">success</button>
+    <button class="btn btn-outline btn-warning">warning</button>
+    <button class="btn btn-outline btn-error">error</button>
+  </div>
+  <div style="margin-top: 10px">
+    <button class="btn btn-lg">large</button>
+    <button class="btn">medium</button>
+    <button class="btn btn-sm">small</button>
+    <button class="btn btn-xs">tiny</button>
   </div>
 </template>
