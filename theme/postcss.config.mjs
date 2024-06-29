2d30fa1dcdb3b17f873b0930eea7b1bd1043b846
xionkq <3030199850@qq.com>
2024-06-29 22:57:41 +0800

    refactor: use postcss-functions

--- theme/postcss.config.mjs
+++ theme/postcss.config.mjs
@@ -5,6 +5,21 @@ const config = {
     'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-functions': {
+      functions: {
+        // 在这里定义您的自定义函数
+        darken: (color, amount = '10%') => {
+          return `color-mix(in hsl, ${color}, black ${amount})`;
+        },
+        lighten: (color, amount) => {
+          return `color-mix(in hsl, ${color}, white ${amount})`;
+        },
+        transparentize: (color, amount = '20%') => {
+          return `color-mix(in hsl, ${color}, transparent ${amount})`;
+        },
+        // 可以添加更多自定义函数
+      }
+    }
   },
 }
 
