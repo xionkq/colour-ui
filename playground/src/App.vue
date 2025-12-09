be7e8d9ad8aaa5be3bb80bbe5ccd4393f001fcc4
xionkq <3030199850@qq.com>
2025-12-09 22:36:37 +0800

    feat: update playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,27 +1,53 @@
 <script setup lang="ts">
 import * as monaco from 'monaco-editor'
-import {onMounted, ref} from "vue";
+import { onMounted, ref, watch } from "vue"
 
-const el = ref<HTMLElement>();
+const editorRef = ref<HTMLElement>()
+const code = ref<string>("<button class=\"btn btn-info\">Info</button>")
 
 onMounted(() => {
-  monaco.editor.create(el.value!, {
-    value: "console.log('hello world')",
-    language: "typescript",
-    dimension: {
-      height: 800,
-      width: 800,
-    },
-  });
-});
+  const editor = monaco.editor.create(editorRef.value!, {
+    value: code.value,
+    language: "html",
+    minimap: {
+      enabled: false,
+    }
+  })
+
+  const handler = debounce(() => {
+    code.value = editor.getValue()
+  }, 1000)
+
+  editor.onDidChangeModelContent(handler)
+})
+
+function debounce(func: Function, wait: number) {
+  let timer: number
+  return (...args: any[]) => {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => func(args), wait);
+  }
+}
+
+watch(code, () => {
+  console.log(code.value)
+})
 </script>
 
 <template>
-  <h1 ref="el">You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="h-screen flex flex-col">
+    <header class="h-15 flex items-center p-3 border-b">colour-ui-playground</header>
+    <main class="main flex-1 flex">
+      <div ref="editorRef" class="w-full"></div>
+      <div class="w-full" v-html="code"></div>
+    </main>
+    <footer class="footer"></footer>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+@import "tailwindcss";
+@import '@xionkq/colour-ui-theme';
+</style>
